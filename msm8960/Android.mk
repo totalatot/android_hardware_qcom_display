452f76ddcee3f908cf5d5ad7f2272d8979c21b1b
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-01-21 23:06:45 +0100

    display: add TARGET_PROVIDES_LIBLIGHT
    Change-Id: I9388f3582d74b4b569e2807e4ec6177dc50f38d0

--- msm8960/Android.mk
+++ msm8960/Android.mk
@@ -1,6 +1,10 @@
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 display-hals += libmemtrack
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+    display-hals += liblight
+endif
+
 ifneq (,$(call is-vendor-board-qcom))
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
