6cc0ec42758d8fee2f87134710c2a45471b8ef43
Pullakavi Srinivas <spullaka@codeaurora.org>
2017-05-03 13:22:36 +0530

    msm8994: qdutils: Generate system image copy for qdmetadata
    Change-Id: I5e222fb259bb1525b02be5f6b44568b9120105a6

--- msm8994/libqdutils/Android.mk
+++ msm8994/libqdutils/Android.mk
@@ -21,7 +21,7 @@ include $(CLEAR_VARS)
 
 LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SHARED_LIBRARIES          := liblog libcutils libhardware libutils
-LOCAL_SRC_FILES                 := qdMetaData.cpp
+LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\" -Wno-sign-conversion
 LOCAL_MODULE_TAGS               := optional
@@ -29,4 +29,19 @@ LOCAL_MODULE                    := libqdMetaData
 LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_LICENSE_KINDS             := SPDX-license-identifier-BSD
 LOCAL_LICENSE_CONDITIONS        := notice
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
+LOCAL_SHARED_LIBRARIES          := liblog libcutils
+LOCAL_C_INCLUDES                := $(common_includes)
+LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
+LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
+LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
+LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
+
+LOCAL_MODULE_TAGS               := optional
+LOCAL_MODULE                    := libqdMetaData.system
+
 include $(BUILD_SHARED_LIBRARY)
